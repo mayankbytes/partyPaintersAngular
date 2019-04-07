399cf5ece03ee4ab740d231f2b7c603ae5ad79ef
Mayank Gupta <mayankgupta@Mayanks-MacBook-Pro.local>
2019-04-08 00:35:11 +0530

    images added to assests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PartyPaintersComponent } from './party-painters/party-painters.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PartyPaintersComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/party-painters/party-painters.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-party-painters',
+  templateUrl: './party-painters.component.html',
+  styleUrls: ['./party-painters.component.css']
+})
+export class PartyPaintersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

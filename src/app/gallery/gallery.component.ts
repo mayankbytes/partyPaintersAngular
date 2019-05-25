41c14b8f8f7949b3c0f0634972623598fbb7dbcd
Mayank Gupta <mayankgupta@Mayanks-MacBook-Pro.local>
2019-05-25 21:02:18 +0530

    gallery pictures added

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -14,14 +14,21 @@ export class GalleryComponent implements OnInit {
 
   ngOnInit() {
     this.images = [];
-    this.images.push({ source: '../../assets/wedding.jpeg', alt: 'Description for Image 1', title: 'Title 1' });
-    this.images.push({ source: '../../assets/wedding.jpeg', alt: 'Description for Image 2', title: 'Title 2' });
-    this.images.push({ source: '../../assets/wedding.jpeg', alt: 'Description for Image 1', title: 'Title 1' });
-    this.images.push({ source: '../../assets/wedding.jpeg', alt: 'Description for Image 2', title: 'Title 2' });
-    this.images.push({ source: '../../assets/wedding.jpeg', alt: 'Description for Image 1', title: 'Title 1' });
-    this.images.push({ source: '../../assets/wedding.jpeg', alt: 'Description for Image 2', title: 'Title 2' });
-    this.images.push({ source: '../../assets/wedding.jpeg', alt: 'Description for Image 1', title: 'Title 1' });
-    this.images.push({ source: '../../assets/wedding.jpeg', alt: 'Description for Image 2', title: 'Title 2' });
+    this.images.push({ source: '../../assets/gallery/1.jpg', alt: 'Description for Image 1', title: 'Title 1' });
+    this.images.push({ source: '../../assets/gallery/2.jpg', alt: 'Description for Image 2', title: 'Title 2' });
+    this.images.push({ source: '../../assets/gallery/3.jpg', alt: 'Description for Image 1', title: 'Title 1' });
+    this.images.push({ source: '../../assets/gallery/4.jpg', alt: 'Description for Image 2', title: 'Title 2' });
+    this.images.push({ source: '../../assets/gallery/5.jpg', alt: 'Description for Image 1', title: 'Title 1' });
+    this.images.push({ source: '../../assets/gallery/6.jpg', alt: 'Description for Image 2', title: 'Title 2' });
+    this.images.push({ source: '../../assets/gallery/7.jpg', alt: 'Description for Image 1', title: 'Title 1' });
+    this.images.push({ source: '../../assets/gallery/8.jpg', alt: 'Description for Image 2', title: 'Title 2' });
+    this.images.push({ source: '../../assets/gallery/9.jpg', alt: 'Description for Image 1', title: 'Title 1' });
+    this.images.push({ source: '../../assets/gallery/10.jpg', alt: 'Description for Image 2', title: 'Title 2' });
+    this.images.push({ source: '../../assets/gallery/11.jpg', alt: 'Description for Image 1', title: 'Title 1' });
+    this.images.push({ source: '../../assets/gallery/12.jpg', alt: 'Description for Image 2', title: 'Title 2' });
+    this.images.push({ source: '../../assets/gallery/13.jpg', alt: 'Description for Image 1', title: 'Title 1' });
+    this.images.push({ source: '../../assets/gallery/14.jpg', alt: 'Description for Image 2', title: 'Title 2' });
+    this.images.push({ source: '../../assets/gallery/15.jpg', alt: 'Description for Image 1', title: 'Title 1' });
   }
 
 }
